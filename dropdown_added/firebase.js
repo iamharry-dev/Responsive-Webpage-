8f49b9fa559195cf2a763a7c4c97e354c9d32f9c
Hariprasath <99019100+iamharry-dev@users.noreply.github.com>
2025-03-21 08:59:04 +0530

    Add files via upload

---
+++ dropdown_added/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBo6stmR8IiPMZTAiZi-W1Wkg21HY213nQ",
+    authDomain: "fraction-app.firebaseapp.com",
+    databaseURL: "https://fraction-app-default-rtdb.firebaseio.com",
+    projectId: "fraction-app",
+    storageBucket: "fraction-app.appspot.com",
+    messagingSenderId: "481746138042",
+    appId: "1:481746138042:web:c2f4558f301458e106a383",
+    measurementId: "G-733PHS2S1Q"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "",
+    authDomain: "fraction-app.firebaseapp.com",
+    databaseURL: "https://fraction-app-default-rtdb.firebaseio.com",
+    projectId: "fraction-app",
+    storageBucket: "fraction-app.appspot.com",
+    messagingSenderId: "481746138042",
+    appId: "1:481746138042:web:c2f4558f301458e106a383",
+    measurementId: "G-733PHS2S1Q"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
